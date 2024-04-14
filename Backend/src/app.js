253846bb9a597a4ef4599fcd35de418026b96360
Manish Joshi <66784945+manish7532@users.noreply.github.com>
2024-04-14 13:49:08 +0530

    Add files via upload

---
+++ Backend/src/app.js
@@ -0,0 +1,191 @@
+import express from 'express';
+import { Mongoose, User, Organization } from './model/db.js';
+import morgan from 'morgan';
+import cors from 'cors';
+import bcrypt from 'bcryptjs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import formidable from 'formidable';
+import fs from 'fs';
+import dotenv from 'dotenv';
+import { Country } from 'country-state-city';
+
+dotenv.config();
+
+const app = express();
+
+Mongoose.connect(process.env.MONGO_URI);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const corsOptions = {
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+});
+
+
+//register form post req
+app.post('/api/register', (req, res) => {
+    const form = formidable({
+        keepExtensions: true,
+        allowEmptyFiles: true,
+        minFileSize: 0
+    });
+
+    form.parse(req, async (err, fields, files) => {
+        if (err) {
+            console.error('Formidable parsing error:', err);
+            return res.status(500).send({ error: 'Error parsing form data' });
+        }
+
+
+        const email = fields.email[0];
+        const firstname = fields.firstname[0];
+        const lastname = fields.lastname[0];
+        const countryCode = fields.country[0];
+        const type = fields.type[0];
+        const password = fields.password[0];
+        const existingUser = await User.findOne({ email: email })
+        if (existingUser) {
+            console.log(existingUser)
+            return res.json(1)
+        }
+
+        if (!email || !firstname || !lastname || !type || !password) {
+            return res.status(400).json(2);
+        }
+
+        try {
+            const hashedPassword = await bcrypt.hash(password, 10);
+
+            const countryData = Country.getCountryByCode(countryCode);
+            const countryName = countryData ? countryData.name : '';
+            const timezone = countryData?.timezones ? countryData.timezones[0].gmtOffsetName : '';
+
+            const newUser = new User({
+                email,
+                firstname,
+                lastname,
+                country: countryName,
+                timezone,
+                type,
+                password: hashedPassword,
+            });
+
+            await newUser.save();
+
+            // Create a folder for the user using their ID
+            const userFolderPath = path.join(__dirname, '../uploads', newUser._id.toString());
+            if (!fs.existsSync(userFolderPath)) {
+                fs.mkdirSync(userFolderPath);
+            }
+
+            // Move the profile photo to the user's folder
+            if (files.profilePhoto) {
+                const file = files.profilePhoto;
+                console.log(file)
+                const newFilePath = path.join(userFolderPath, file[0].originalFilename);
+                await fs.promises.rename(file[0].filepath, newFilePath);
+
+                // Update user profile photo path
+                await User.updateOne({ _id: newUser._id }, { $set: { profilePhoto: newFilePath } })
+            }
+
+            // Respond with success message
+            res.status(201).json(3);
+
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({ error: 'Error saving user' });
+        }
+    });
+});
+
+
+//org form post req
+app.post('/api/organization', (req, res) => {
+    // Create a new formidable form instance
+    const form = formidable({
+        keepExtensions: true,
+        allowEmptyFiles: true,
+        minFileSize: 0,
+    });
+
+    // Parse the incoming form data
+    form.parse(req, async (err, fields, files) => {
+        if (err) {
+            console.error('Formidable parsing error:', err);
+            return res.status(500).send({ error: 'Error parsing form data' });
+        }
+
+        // Extract form fields
+        const { email, org_Name, domain } = fields;
+        const logoFile = files.logo ? files.logo[0] : null;
+        // console.log(logoFile)
+        // console.log("--------------------------------------------------------------------------------------------")
+        // console.log(email, org_Name, domain)
+        // console.log("--------------------------------------------------------------------------------------------")
+        // console.log(fields.email[0])
+        // Validate required fields
+        if (!email || !org_Name || !domain) {
+            return res.status(400).json({ error: 'Missing required fields' });
+        }
+
+        const existingUser = await User.findOne({ email: email[0] });
+
+        if (!existingUser) {
+            return res.json("User not found");
+        }
+
+        try {
+            // Create a new organization record
+            const newOrganization = new Organization({
+                userID: existingUser._id,
+                org_Name:org_Name[0],
+                domain:domain[0],
+                logo: '', // Logo will be updated later
+            });
+
+            // Save the organization to the database
+            await newOrganization.save();
+
+            // Create a folder for the organization using its ID
+            const orgFolderPath = path.join(__dirname, '../Org_logo', newOrganization._id.toString());
+            if (!fs.existsSync(orgFolderPath)) {
+                fs.mkdirSync(orgFolderPath);
+            }
+
+            // If there's a logo file, save it in the organization's folder
+            if (logoFile) {
+                const logoFilePath = path.join(orgFolderPath, logoFile.originalFilename);
+                await fs.promises.rename(logoFile.filepath, logoFilePath);
+
+                // Update the organization's logo path in the database
+                newOrganization.logo = logoFilePath;
+                await newOrganization.save();
+            }
+
+            // Respond with success message
+            res.status(201).json({ message: 'Organization registered successfully', organization: newOrganization });
+
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({ error: 'Error saving organization' });
+        }
+    });
+});
+
---
+++ Backend/src/model/db.js
@@ -0,0 +1,118 @@
+import Mongoose from 'mongoose';
+const Schema = Mongoose.Schema;
+
+// Users Schema
+const userSchema = new Schema({
+    email: { type: String, unique: true, required: true },
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    profilePhoto: { type: String, default: '' },
+    country: { type: String },
+    timezone: { type: String },
+    type: { type: String, enum: ['Organization', 'Individual'], required: true },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+// Organizations Schema
+const organizationSchema = new Schema({
+    userID: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    org_Name: { type: String, required: true },
+    domain: { type: String },
+    logo: { type: String },
+    theme: { type: String }
+});
+
+// Events Schema
+const eventSchema = new Schema({
+    title: { type: String, required: true },
+    description: { type: String },
+    startDate: { type: Date },
+    endDate: { type: Date },
+    duration: { type: Number },
+    organizerId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    Time: { type: Date },
+    timeZone: { type: String },
+    registrationCriteria: { type: String },
+    Recordings: { type: Boolean },
+    QnA: { type: Boolean },
+    chatType: { type: String, enum: ['All', 'Host', 'Persontoperson'] },
+    Feedback: { type: Boolean },
+    WaitingRoom: { type: Boolean },
+    privacySetting: { type: String, enum: ['public', 'private'] },
+    participantLimit: { type: Number }
+});
+
+// Participants Schema
+const participantSchema = new Schema({
+    eventID: { type: Schema.Types.ObjectId, ref: 'Event', required: true },
+    userID: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    joinedAt: [{ type: Date }],
+    leftAt: [{ type: Date }],
+    duration: { type: Number },
+    matchCriteria: { type: Boolean },
+    role: { type: String, enum: ['host', 'co-host', 'moderator', 'user'] }
+});
+
+// InteractiveFeatures Schema
+const interactiveFeaturesSchema = new Schema({
+    eventID: { type: Schema.Types.ObjectId, ref: 'Event', required: true },
+    userID: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    responsesToPoll: { type: Boolean },
+    noOfQuestions: { type: Boolean },
+    liveReactions: { type: Boolean },
+    engagementMetrics: { type: Boolean }
+});
+
+// Recordings Schema
+const recordingSchema = new Schema({
+    eventID: { type: Schema.Types.ObjectId, ref: 'Event', required: true },
+    pathToVideo: { type: String },
+    Date: { type: Date }
+});
+
+// Feedbacks Schema
+const feedbackSchema = new Schema({
+    userID: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    eventID: { type: Schema.Types.ObjectId, ref: 'Event', required: true },
+    rating: { type: Number },
+    audioQuality: { type: Number },
+    videoQuality: { type: Number },
+    audienceSatisfaction: { type: Number },
+    suggestion: { type: String }
+});
+
+// Analytics Schema
+const analyticsSchema = new Schema({
+    eventID: { type: Schema.Types.ObjectId, ref: 'Event', required: true },
+    interactiveFeaturesID: { type: Schema.Types.ObjectId, ref: 'InteractiveFeatures' },
+    feedbacksID: { type: Schema.Types.ObjectId, ref: 'Feedbacks' },
+    audienceSatisfaction: { type: Number },
+    engagementMetrics: { type: Number },
+    successRate: { type: String }
+});
+
+// models
+const User = Mongoose.model('User', userSchema);
+const Organization = Mongoose.model('Organization', organizationSchema);
+const Event = Mongoose.model('Event', eventSchema);
+const Participant = Mongoose.model('Participant', participantSchema);
+const InteractiveFeatures = Mongoose.model('InteractiveFeatures', interactiveFeaturesSchema);
+const Recording = Mongoose.model('Recording', recordingSchema);
+const Feedback = Mongoose.model('Feedback', feedbackSchema);
+const Analytics = Mongoose.model('Analytics', analyticsSchema);
+
+//exporting models
+export {
+    Mongoose,
+    User,
+    Organization,
+    Event,
+    Participant,
+    InteractiveFeatures,
+    Recording,
+    Feedback,
+    Analytics
+};
